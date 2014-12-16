2a1f87a53fb81d8b6dbcd6a9461dd4b17a601461
Eric Saxby <eric@wanelo.com>
2014-12-15 18:01:51 -0800

    5.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@wanelo.com'
 license 'Apache 2.0'
 description 'Installs/Configures solr.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '5.0.1'
+version '5.0.2'
 
 depends 'ipaddr_extensions'
 depends 'java'
